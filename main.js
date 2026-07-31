47aee1dda80fb06e0f41d0a3a0c38e3cbed733fb
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-01 00:07:38 +0800

    refactor: store path in path object

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function knightMoves(start, end) {
+function validMoves(position) {
   let movesOfKnight = [
     [2, -1],
     [2, 1],
@@ -9,17 +9,54 @@ function knightMoves(start, end) {
     [-2, -1],
     [-2, 1],
   ];
+  let result = [];
   for (let i = 0; i < movesOfKnight.length; i++) {
     const val = [
-      movesOfKnight[i][0] + start[0],
-      movesOfKnight[i][1] + start[1],
+      movesOfKnight[i][0] + position[0],
+      movesOfKnight[i][1] + position[1],
     ];
-
     if (val[0] > 7 || val[0] < 0 || val[1] > 7 || val[1] < 0) {
       continue;
     }
-    console.log(val);
+    result.push(val);
   }
+  return result;
 }
 
-console.log(knightMoves([7, 0]));
+// function knightMoves(start, destination) {
+//   let visited = [start];
+//   let queue = [[start]];
+//   let count = 0;
+
+//   validMoves(queue[count][queue[count].length - 1]).forEach((move) => {
+//     const curr = queue[count];
+//     const x = queue.push([curr[0], move]);
+//   });
+
+//   console.log(queue);
+// }
+
+function knightMoves(start, destination) {
+  let visited = [start];
+  let queue = [{ current: start, path: [start] }];
+  let count = 0;
+
+  while (queue.length < 50) {
+    const current = queue[count].path;
+
+    validMoves(queue[count].current).forEach((move) => {
+      queue.push({ current: move, path: [...current, move] });
+    });
+    count++;
+  }
+}
+knightMoves([0, 0]);
+
+const x = [
+  [0, 0],
+  [2, 1],
+];
+
+const y = [...x, [2, 2]];
+
+// console.log(y);
