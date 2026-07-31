87aa8a449ac0f9e006205a9c0b648b4fdfea73e4
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-01 01:12:25 +0800

    solved: return all result from 00 to 77

--- main.js
+++ main.js
@@ -37,26 +37,43 @@ function validMoves(position) {
 // }
 
 function knightMoves(start, destination) {
-  let visited = [start];
+  let hasVisited = [start];
   let queue = [{ current: start, path: [start] }];
   let count = 0;
 
-  while (queue.length < 50) {
+  while (queue[count]) {
     const current = queue[count].path;
 
     validMoves(queue[count].current).forEach((move) => {
+      const hasMatch = hasVisited.some(
+        (subArr) =>
+          subArr.length === 2 && subArr[0] === move[0] && subArr[1] === move[1],
+      );
+      hasVisited.push(move);
+      if (hasMatch) return;
       queue.push({ current: move, path: [...current, move] });
     });
     count++;
   }
+  queue.forEach((move) => {
+    console.log(move.path);
+  });
+  // console.log(visited);
 }
-knightMoves([0, 0]);
 
-const x = [
+const test = [
   [0, 0],
-  [2, 1],
+  [1, 1],
+  [2, 2],
 ];
 
-const y = [...x, [2, 2]];
+const hasMatch = test.some(
+  (subArr) => subArr.length === 2 && subArr[0] === 0 && subArr[1] === 0,
+);
 
-// console.log(y);
+console.log(hasMatch);
+// function check(x, y) {
+//   return x[0] === y[0] && x[1] === y[1];
+// }
+
+knightMoves([0, 0]);
