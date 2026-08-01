8301ca81a114b9f183b724bf684b1586505a6a81
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-08-01 13:00:33 +0800

    refactor: change current variable name to path to avoid confusion and added comments for code readers

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// All the possible moves("L moves") of a knight is generated here.
 function validMoves(position) {
   let movesOfKnight = [
     [2, -1],
@@ -23,35 +24,50 @@ function validMoves(position) {
   return result;
 }
 
+// Checks if 2 array value is same eg.. [2,2] === [2,2] is true
 function checkIfSameValue(x, y) {
   return x[0] === y[0] && x[1] === y[1];
 }
 
+// All the path from start to destination is generated here
 function knightPath(start, destination) {
+  // "hasWalked" if a node or a square has been walked or visited it will be stored here. To avoid being walked again. Its to avoid looping forever
   const hasWalked = new Set();
   hasWalked.add(`${start[0]},${start[1]}`);
 
+  // store queue(BFS)
   let queue = [{ current: start, path: [start] }];
   let count = 0;
 
   while (queue[count]) {
-    const current = queue[count].path;
-    if (checkIfSameValue(queue[count].current, destination)) {
+    // Get the current path and check if we reached the destination.
+    const path = queue[count].path;
+    const curr = checkIfSameValue(queue[count].current, destination);
+
+    // Return the shortest path once the destination is found.
+    if (curr) {
       return queue[count].path;
     }
 
+    // explore all valid knight moves from the current position.
     validMoves(queue[count].current).forEach((move) => {
       const hasMatch = `${move[0]},${move[1]}`;
 
+      // if the move in valid knight is in hasWalker(this stores the square we walked or visited) return
       if (hasWalked.has(hasMatch)) return;
+      // if not in hasWalked store this in there
       hasWalked.add(hasMatch);
-      queue.push({ current: move, path: [...current, move] });
+
+      // pushes to queue eg. {current: [2,2], path: [[0,0],[2,2]]}
+      queue.push({ current: move, path: [...path, move] });
     });
 
+    // Move to the next position in the BFS queue.
     count++;
   }
 }
 
+// checks for errors in passed values in start and destination if theres no problem display the shortest path
 function knightMoves(start, destination) {
   if (!start) {
     console.log(`No Start Value`);
@@ -83,8 +99,18 @@ function knightMoves(start, destination) {
     return;
   }
 
+  // if path found
   const result = knightPath(start, destination);
 
+  /*
+  This displays the path 
+  output eg. You made it in 4 moves!  Here's your path:
+  [ 3, 3 ]
+  [ 5, 2 ]
+  [ 7, 3 ]
+  [ 6, 5 ]
+  [ 7, 7 ]
+ */
   console.log(`You made it in ${result.length - 1} moves!  Here's your path:`);
   result.forEach((move) => {
     console.log(move);
